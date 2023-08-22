fd5c8a0cf103523527f0f7596ec3b81446d2ae38
fisku <fisk@fisk.lan>
2023-08-22 02:13:26 -0300

    Added wallpaper to autostart

--- config.def.h
+++ config.def.h
@@ -29,6 +29,7 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
         "st", NULL,
         "picom", "--config", "/home/fisk/.local/picom.conf", NULL,
+	"xwallpaper", "--center", "/home/fisk/.local/wallpaper.jpg", NULL,
         "dwmblocks", NULL,
         NULL /* terminate */
 };
