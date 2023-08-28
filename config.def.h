0fd22ce9b85ae1063f098b6bc047cd87afe6a6a2
fisk pringles <fisk@compu.lan>
2023-08-28 01:13:33 -0300

    theme fix

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-#include "themes/cyber.h"
+#include "themes/kanagawa.h"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
