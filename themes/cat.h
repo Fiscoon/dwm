e1e8430b092b444357e99d25bebfab5d3658d103
fisk pringles <fisk@compu.lan>
2023-08-30 23:13:16 -0300

    Added purple theme

---
+++ themes/cat.h
@@ -0,0 +1,5 @@
+static const char col_foreground[]    = "#d2d1f7";
+static const char col_background[]    = "#363347";
+static const char col_yellow[]        = "#f0caa6";
+static const char col_white[]         = "#ffffff";
+static const char col_cyan[]          = "#6A9589";
