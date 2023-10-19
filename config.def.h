6379055fc66be60c9e9d200a8ee595098f9f9b34
ness <ness@thinkpadx220i.my.domain>
2023-10-18 22:47:40 -0300

    Added bookmark search through dmenu

--- config.def.h
+++ config.def.h
@@ -100,7 +100,8 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	// { MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      spawn,          SHCMD("xdotool type --delay 0 $(cat $HOME/.local/share/bookmarks.txt | dmenu -l 30 | cut -d ' ' -f 1)") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
