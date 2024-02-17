3afad7f065b12a275d7fe18740def3e17ce1aa38
ness <ness@thinkpadx220i.my.domain>
2024-02-17 16:59:22 -0300

    Changed bookmarks location

--- config.def.h
+++ config.def.h
@@ -101,7 +101,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	// { MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_b,      spawn,          SHCMD("xdotool type --delay 0 $(cat $HOME/.local/share/bookmarks.txt | dmenu -l 30 | cut -d ' ' -f 1)") },
+	{ MODKEY,                       XK_b,      spawn,          SHCMD("xdotool type --delay 0 $(cat $HOME/.local/bookmarks.txt | dmenu -l 30 | cut -d ' ' -f 1)") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
